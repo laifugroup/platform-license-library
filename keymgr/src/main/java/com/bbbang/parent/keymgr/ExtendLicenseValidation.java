9de8bbfad0dce8fc964e43ea325885b217c9a681
jiliapp <jilijili@jiliapp.cn>
2023-03-10 14:09:15 +0800

    license

---
+++ keymgr/src/main/java/com/bbbang/parent/keymgr/ExtendLicenseValidation.java
@@ -0,0 +1,12 @@
+package com.bbbang.parent.keymgr;
+
+import global.namespace.truelicense.api.License;
+import global.namespace.truelicense.api.LicenseValidation;
+import global.namespace.truelicense.api.LicenseValidationException;
+
+public class ExtendLicenseValidation implements LicenseValidation {
+    @Override
+    public void validate(License license) throws LicenseValidationException {
+
+    }
+}
